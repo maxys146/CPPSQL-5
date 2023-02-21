0f0d4c2950c9e2720e3c3ed0a15c6d73a1666f02
max-asus <maxys146@gmail.com>
2023-02-21 21:32:11 +0300

    Сделал удаление и создание БД

--- sqlcpp-5.1/sqlcpp-5.1.cpp
+++ sqlcpp-5.1/sqlcpp-5.1.cpp
@@ -9,11 +9,34 @@ int main()
 {
 	try
 	{
+		setlocale(LC_ALL, "ru_RU.UTF-8");
+
 		pqxx::connection c("host=localhost "
 			"port=5432 "
 			"dbname=netology05 "
 			"user=postgres "
 			"password=111111");
+
+		// Создаем БД
+		pqxx::work tx{ c };
+		tx.exec("DROP TABLE IF EXISTS public.Phones");
+		tx.exec("DROP TABLE IF EXISTS public.Clients");
+		tx.exec("CREATE TABLE public.Clients("
+			"id SERIAL PRIMARY KEY, "
+			"\"firstname\" VARCHAR NOT NULL,"
+			"\"lastname\" VARCHAR NOT NULL,"
+			"\"email\" VARCHAR NOT NULL)");
+		tx.exec("CREATE TABLE public.Phones("
+			"\"id\" SERIAL PRIMARY KEY,"
+			"\"phone\" VARCHAR NOT NULL,"
+			"\"client\" INTEGER REFERENCES public.Clients(id))");
+
+		tx.commit();
+
+
+
+
+
 	}
 	catch (const pqxx::sql_error& e)
 	{
@@ -21,8 +44,7 @@ int main()
 	}
 	catch (...)
 	{
-	std::cout << "Some error.." << std::endl;
-
+		std::cout << "Some error.." << std::endl;
 	}
 	return 0;
 }
