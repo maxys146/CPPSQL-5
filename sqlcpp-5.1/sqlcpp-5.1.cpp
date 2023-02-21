459f44e2f2c20c22ffd3f0b849439b3aef4dfd57
max-asus <maxys146@gmail.com>
2023-02-21 22:17:34 +0300

    Сделал заготовку для класса

--- sqlcpp-5.1/sqlcpp-5.1.cpp
+++ sqlcpp-5.1/sqlcpp-5.1.cpp
@@ -4,12 +4,30 @@
 #include "sqlcpp-5.1.h"
 #include <pqxx/pqxx>
 
+class Client
+{
+public:
+	void addClient(std::string asd)
+	{
+		std::cout << "Добавляем клиента: " << asd << "\n";
+		//return 1324;
+	}
+
+	// TODO Конструктор
+};
 
 int main()
 {
 	try
 	{
-		setlocale(LC_ALL, "ru_RU.UTF-8");
+		//setlocale(LC_ALL, "ru_RU.UTF-8");
+		setlocale(LC_ALL, "Russian");
+
+		Client* client = new Client;
+		//std::cout << client->addClient() << std::endl;
+		client->addClient("Курочкин");
+
+		return 0;
 
 		pqxx::connection c("host=localhost "
 			"port=5432 "
@@ -19,14 +37,14 @@ int main()
 
 		// Создаем БД
 		pqxx::work tx{ c };
-		tx.exec("DROP TABLE IF EXISTS public.Phones");
-		tx.exec("DROP TABLE IF EXISTS public.Clients");
-		tx.exec("CREATE TABLE public.Clients("
+		//tx.exec("DROP TABLE IF EXISTS public.Phones");
+		//tx.exec("DROP TABLE IF EXISTS public.Clients");
+		tx.exec("CREATE TABLE IF NOT EXISTS public.Clients("
 			"id SERIAL PRIMARY KEY, "
 			"\"firstname\" VARCHAR NOT NULL,"
 			"\"lastname\" VARCHAR NOT NULL,"
 			"\"email\" VARCHAR NOT NULL)");
-		tx.exec("CREATE TABLE public.Phones("
+		tx.exec("CREATE TABLE IF NOT EXISTS public.Phones("
 			"\"id\" SERIAL PRIMARY KEY,"
 			"\"phone\" VARCHAR NOT NULL,"
 			"\"client\" INTEGER REFERENCES public.Clients(id))");
